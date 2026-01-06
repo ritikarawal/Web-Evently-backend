d0cddf4c24314df91583e850401ffc87057d651a
@ririkarawal <rawalritika342@gmail.com>
2026-01-06 10:28:51 +0530

    repository layer and service logics

---
+++ src/services/user.service.ts
@@ -0,0 +1,52 @@
+import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { UserRepository } from "../repositories/user.repository";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { HttpError } from "../errors/http-error";
+
+const userRepo = new UserRepository();
+
+export class UserService {
+    async register(data: CreateUserDTO) {
+        const emailExists = await userRepo.getUserByEmail(data.email);
+        if (emailExists) {
+            throw new HttpError(400, "Email already exists");
+        }
+
+        const usernameExists = await userRepo.getUserByUsername(data.username);
+        if (usernameExists) {
+            throw new HttpError(400, "Username already exists");
+        }
+
+        const hashedPassword = await bcrypt.hash(data.password, 10);
+
+        return await userRepo.createUser({
+            ...data,
+            password: hashedPassword,
+        });
+    }
+
+    async login(data: LoginUserDTO) {
+        const user = await userRepo.getUserByEmail(data.email);
+        if (!user) {
+            throw new HttpError(404, "User not found");
+        }
+
+        const validPassword = await bcrypt.compare(
+            data.password,
+            user.password
+        );
+        if (!validPassword) {
+            throw new HttpError(401, "Invalid credentials");
+        }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            JWT_SECRET,
+            { expiresIn: "30d" }
+        );
+
+        return { token, user };
+    }
+}
