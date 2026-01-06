5af1f6f5c7db549cc66d35c83c21a8e1137b33b0
@ririkarawal <rawalritika342@gmail.com>
2026-01-06 10:03:10 +0530

    mongodb connection

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
-export const config = {
-  port: Number(process.env.PORT) || 5000,
-  mongoUri: process.env.MONGO_URI as string,
-  jwtSecret: process.env.JWT_SECRET as string,
-};
+export const PORT: number =
+    process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+export const MONGODB_URI: string =
+    process.env.MONGODB_URI || "mongodb://localhost:27017/evently";
+
+export const JWT_SECRET: string =
+    process.env.JWT_SECRET || "secret";
