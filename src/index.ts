a02b4ad0584edd81bd99aa8dcd9d38f76fab1c6a
@ririkarawal <rawalritika342@gmail.com>
2026-01-20 15:20:02 +0545

    handled cors

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import { connectDatabase } from "./database/mongodb";
 import authRoutes from "./routes/auth.route";
 import eventRoutes from "./routes/event.route";
 import { PORT } from "./config";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
 
